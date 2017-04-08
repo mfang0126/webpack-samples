8c284a8a8beea4487cf2c15fef9c8926a7f117b4
Freedom <fang.ming@zoho.com>
2017-04-08 16:47:15 +1200

    Updated with sass bootstrap customization.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 var path = require('path');
+var bootstrapEntryPoints = require('./webpack.bootstrap.config');
 var isProd = process.env.NODE_ENV === 'production';
 var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 var cssProd = ExtractTextPlugin.extract({
@@ -10,10 +11,13 @@ var cssProd = ExtractTextPlugin.extract({
     publicPath: '/dist'
 });
 var cssConfig = isProd ? cssProd : cssDev;
+var bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
 module.exports = {
     entry: {
         home: './src/home.js',
-        contact: './src/contact.js'
+        contact: './src/contact.js',
+        bootstrap: bootstrapConfig
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -23,6 +27,18 @@ module.exports = {
         rules: [{
             test: /\.scss$/,
             use: cssConfig
+        },
+        {
+            test: /\.(woff2?|svg)$/, 
+            use: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+        },
+        {
+            test: /\.(ttf|eot)$/,
+            use: 'file-loader?name=fonts/[name].[ext]'
+        },
+        { 
+            test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, 
+            use: 'imports-loader?jQuery=jquery' 
         }]
     },
     devServer: {
@@ -48,7 +64,7 @@ module.exports = {
             hash: true
         }),
         new ExtractTextPlugin({
-            filename: 'home.css',
+            filename: '/css/[name].css',
             disable: !isProd,
             allChunks: true,
         }),
